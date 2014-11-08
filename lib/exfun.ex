71eda70a05ef6a5629176972c6342cda15c4e93c
miwee <nouser@nodomain.com>
2014-11-08 13:16:11 +0530

    added put_unless_nil

--- lib/exfun.ex
+++ lib/exfun.ex
@@ -1,26 +1,37 @@
-defmodule Exfun.Utils do
-end
-
-defmodule Exfun.MacroUtils do
-end
-
-defmodule Exfun.Enum do
-end
-
-defmodule Exfun.Kernel do
+defmodule Exfun do
   @doc """
   Pipes `val` through `fun`, useful with |> operator
   for transforming single (non-list) return values 
   from previous pipeline stage.
   ## Examples
 
-  iex(1)> import Exfun.Kernel
-  nil
-  iex(2)> %{a: 4} |> pipe_fn(fn x -> x.a end) 
+  iex(1)> %{a: 4} |> Exfun.pipe_fn(fn x -> x.a end) 
   4
 
   """
   def pipe_fn(val, fun) do 
     fun.(val)
   end
+
+  @doc """
+  Put `value` for `key` in Dict `m`, 
+  unless `value` is nil
+  ## Examples
+
+  iex(1)> %{a: 4, b: 5} |> Exfun.put_unless_nil(:a, 6) 
+  %{a: 6, b: 5}
+  iex(2)> %{a: 4, b: 5} |> Exfun.put_unless_nil(:a, nil) 
+  %{a: 4, b: 5}
+
+  """
+  def put_unless_nil(m, _key, nil) when is_map(m) do 
+    m
+  end  
+
+  def put_unless_nil(m, key, value) when is_map(m) do 
+    Dict.put(m, key, value)
+  end  
+end
+
+defmodule Exfun.Macros do
 end
--- test/exfun_test.exs
+++ test/exfun_test.exs
@@ -1,8 +1,7 @@
 defmodule ExfunTest do
   use ExUnit.Case
-  doctest Exfun.Kernel
-  doctest Exfun.Utils
-  doctest Exfun.MacroUtils
+  doctest Exfun
+  doctest Exfun.Macros
 
   test "the truth" do
     assert 1 + 1 == 2
